bb3f06b175b96a83b966266899897a453064f24f
Pirvulescu Catalin <125762622+Elyticus@users.noreply.github.com>
2023-04-11 22:53:28 +0300

    add small changes

---
+++ app.js
@@ -0,0 +1,18 @@
+const images = [
+  "imgs/1.png",
+  "imgs/12.png",
+  "imgs/41.png",
+  "imgs/5.png",
+  "imgs/52.png",
+  "imgs/17.png",
+];
+
+const container = document.getElementById("container");
+
+function renderImg() {
+  for (let i = 0; i < images.length; i++) {
+    container.innerHTML += `<div class="gall-img" style="background-image: url(${images[i]})">`;
+  }
+}
+
+renderImg();
